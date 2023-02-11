d3461a6a95e87b67927ab80032c3b186384bc9c3
Jonathan Silva <jonathan.design16@outlook.com>
2023-02-11 20:30:17 -0300

    novoos_comennts

--- Comentarios.py
+++ Comentarios.py
@@ -6,7 +6,7 @@ fake = Faker()
 positive_comments = [
     "Adorei o lugar, a comida é incrível!",
     "O atendimento foi excelente e o lanche estava delicioso!",
-    "Eu definitivamente gosto e  recomendo!",
+    "Eu definitivamente gosto e recomendo!",
     "Sem dúvida, este é um dos melhores McDonald's que já fui. A comida é incrível e o ambiente é limpo!"
     "O atendimento foi rápido, os atendentes super atenciosos"
     "Lanche gostoso, muito bom nota 10"
@@ -19,7 +19,8 @@ positive_comments = [
     "Local limpo e agradavel, o lanche veio perfeito e quentinho"
     "Gostei do lugar, o lanche veio rapidinho, tudo certo"
     "top dms, mto bom"
-    
+    "McDonald's como sempre muito bom"
+
 
 
 ]
