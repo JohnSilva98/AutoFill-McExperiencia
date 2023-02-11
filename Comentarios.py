7d9539acfee0c24f7b0623b928cc661038390667
Jonathan Silva <jonathan.design16@outlook.com>
2023-02-11 20:28:07 -0300

    novos comentarios

--- Comentarios.py
+++ Comentarios.py
@@ -7,11 +7,20 @@ positive_comments = [
     "Adorei o lugar, a comida é incrível!",
     "O atendimento foi excelente e o lanche estava delicioso!",
     "Eu definitivamente gosto e  recomendo!",
-    "Sem dúvida, este é um dos melhores McDonald's que já fui. A comida é incrível e o ambiente é limpo!",
+    "Sem dúvida, este é um dos melhores McDonald's que já fui. A comida é incrível e o ambiente é limpo!"
     "O atendimento foi rápido, os atendentes super atenciosos"
     "Lanche gostoso, muito bom nota 10"
     "atendimento rápido, lanche mto bom"
+    "restaurante limpo, funcionários simpáticos, comida boa"
+    "tudo top"
+    "top"
+    "top demais, pessoal atencioso"
+    "todos são atenciosos, principalmente com as crianças, meu filho amou"
+    "Local limpo e agradavel, o lanche veio perfeito e quentinho"
+    "Gostei do lugar, o lanche veio rapidinho, tudo certo"
+    "top dms, mto bom"
     
+
 
 ]
 #variavel que recebe o comentario aleatorio
