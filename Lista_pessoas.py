112909ced6b76762c577d95d4139ed562c0985c5
Jonathan Silva <jonathan.design16@outlook.com>
2023-02-18 14:51:04 -0300

    fakert

--- Lista_pessoas.py
+++ Lista_pessoas.py
@@ -11,7 +11,7 @@ for i in range(3):
     first_name = fake.first_name()
     last_name = fake.last_name()
     fullname = re.sub(r"\s+", "-", first_name + " " + last_name)
-    telefone = fake.phone_number()
+    telefone = fake.phone_number(area_code='11')
     email= fullname.lower() + "@gmail.com"
     person = {"nome": first_name, "sobrenome": last_name, "telefone": telefone, "email":email}
     people_list.append(person)
